da002afa18bb6d540e3d68b67cee757fe717a82b
adadvar <a.dadvar_71@yahoo.com>
2024-06-13 13:31:07 +0330

    add default value to select

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -5,99 +5,100 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 
 const schema = z.object({
-  description: z
-    .string()
-    .min(3, { message: "Description should be ad least 3 characters." }),
-  amount: z.number({ invalid_type_error: "Amount is required." }),
-  category: z.string({ required_error: "Category is required." }),
+	description: z
+		.string()
+		.min(3, { message: "Description should be ad least 3 characters." }),
+	amount: z.number({ invalid_type_error: "Amount is required." }),
+	category: z.string({ required_error: "Category is required." }),
 });
 
 const catitem = ["Groceries", "Utilities", "Entertainment"];
 
-const [selectedCategory, setSelectedCategory] = useState("All category");
-
 type FormData = z.infer<typeof schema>;
 
 const Form = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isValid },
-  } = useForm<FormData>({ resolver: zodResolver(schema) });
+	const [selectedCategory, setSelectedCategory] = useState("All category");
+	const {
+		register,
+		handleSubmit,
+		formState: { errors, isValid },
+	} = useForm<FormData>({ resolver: zodResolver(schema) });
 
-  const onSubmit = (data: FieldValues) => {
-    console.log(data);
-  };
+	const onSubmit = (data: FieldValues) => {
+		console.log(data);
+	};
 
-  return (
-    <>
-      {selectedCategory}
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="mb-3">
-          <label htmlFor="description" className="form-label">
-            Description
-          </label>
-          <input
-            {...register("description")}
-            id="description"
-            type="text"
-            className="form-control"
-          />
-          {errors.description && (
-            <p className="text-danger">{errors.description.message}</p>
-          )}
-        </div>
-        <div className="mb-3">
-          <label htmlFor="amount" className="form-label">
-            Amount
-          </label>
-          <input
-            {...register("amount", { valueAsNumber: true })}
-            id="amount"
-            type="number"
-            className="form-control"
-          />
-          {errors.amount && (
-            <p className="text-danger">{errors.amount.message}</p>
-          )}
-        </div>
-        <div className="mb-3">
-          <label htmlFor="category" className="form-label">
-            Category
-          </label>
-          <select
-            {...register("category")}
-            id="category"
-            className="form-select"
-          >
-            <option selected></option>
-            {catitem.map((item) => (
-              <option value={item}>{item}</option>
-            ))}
-          </select>
-          {errors.category && (
-            <p className="text-danger">{errors.category.message}</p>
-          )}
-        </div>
-        <button className="btn btn-primary" type="submit">
-          Submit
-        </button>
-      </form>
+	return (
+		<>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<div className="mb-3">
+					<label htmlFor="description" className="form-label">
+						Description
+					</label>
+					<input
+						{...register("description")}
+						id="description"
+						type="text"
+						className="form-control"
+					/>
+					{errors.description && (
+						<p className="text-danger">{errors.description.message}</p>
+					)}
+				</div>
+				<div className="mb-3">
+					<label htmlFor="amount" className="form-label">
+						Amount
+					</label>
+					<input
+						{...register("amount", { valueAsNumber: true })}
+						id="amount"
+						type="number"
+						className="form-control"
+					/>
+					{errors.amount && <p className="text-danger">{errors.amount.message}</p>}
+				</div>
+				<div className="mb-3">
+					<label htmlFor="category" className="form-label">
+						Category
+					</label>
+					<select
+						{...register("category")}
+						defaultValue={""}
+						id="category"
+						className="form-select"
+					>
+						<option></option>
+						{catitem.map((item) => (
+							<option key={item} value={item}>
+								{item}
+							</option>
+						))}
+					</select>
+					{errors.category && (
+						<p className="text-danger">{errors.category.message}</p>
+					)}
+				</div>
+				<button className="btn btn-primary" type="submit">
+					Submit
+				</button>
+			</form>
 
-      <div className="mb-3 mt-5">
-        <select
-          className="form-select"
-          value={selectedCategory}
-          onChange={(e) => setSelectedCategory(e.target.value)}
-        >
-          <option value="All category">All category</option>
-          {catitem.map((item) => (
-            <option value={item}>{item}</option>
-          ))}
-        </select>
-      </div>
-    </>
-  );
+			<div className="mb-3 mt-5">
+				<select
+					className="form-select"
+					value={selectedCategory}
+					onChange={(e) => setSelectedCategory(e.target.value)}
+				>
+					<option value="All category">All category</option>
+					{catitem.map((item) => (
+						<option key={item} value={item}>
+							{item}
+						</option>
+					))}
+				</select>
+			</div>
+		</>
+	);
 };
 
 export default Form;
